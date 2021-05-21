3c80b39f4b67b8449c119bbbe0ae2179b27eaa11
wong hejun <wonghejun@outlook.com>
2021-05-21 16:35:12 +0800

    Add project files.

---
+++ CronJobWebAPI/Services/CronJob1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CronJobWebAPI.Services
+{
+    public class CronJob1 : CronJobService
+    {
+        private readonly ILogger<CronJob1> _logger;
+
+        public CronJob1(IScheduleConfig<CronJob1> config, ILogger<CronJob1> logger)
+            : base(config.CronExpression, config.TimeZoneInfo)
+        {
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("CronJob 1 starts.");
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task DoWork(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"{DateTime.Now:hh:mm:ss} CronJob 1 is working.");
+            return Task.CompletedTask;
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("CronJob 1 is stopping.");
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
